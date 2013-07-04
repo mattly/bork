1572ca9af336601863a13085256a5fa3845eaf5a
Matthew Lyon <matthew@lyonheart.us>
2013-07-04 16:38:44 -0700

    setup brew taps

--- sources/brew.sh
+++ sources/brew.sh
@@ -3,13 +3,19 @@ brew () {
   local c=''
   if contains "$brews_have" "$pkg" ; then
     if contains "$brews_outdated" "$pkg" ; then
-      c="brew upgrade $pkg"
+      bake "command brew upgrade $pkg"
     fi
   else
-    c="brew install $@"
+    bake "command brew install $@"
   fi
-  if [ -n "$c" ] ; then bake "command $c" ; fi
 }
 brews_have=$(command brew list)
 brews_outdated=$(command brew outdated | awk '{print $1}')
+brew_taps=$(command brew tap)
+
+brew_tap () {
+  if ! contains "$brew_taps" $1 ; then
+    bake "command brew tap $1"
+  fi
+}
 
