a6b49066dd2e156607521aea53d86843e7327242
Matthew Lyon <matthew@lyonheart.us>
2013-07-16 22:22:22 -0700

    actually make git-update work

--- sources/git.sh
+++ sources/git.sh
@@ -9,13 +9,24 @@ git () {
     bake "mkdir -p $dir"
     bake "command git clone $git_url $dir"
   else
-    fetch=$(cd $dir
-            command git fetch --dry-run)
-    if [ -n "$fetch" ]; then
-      bake_at $dir
-      bake "command git pull"
-      bake_at $dir
-      bake "command git log HEAD@{1}.."
+    echo $git_url
+    fetch=$(cd $dir; command git fetch --dry-run 2>&1 )
+    if matches "$fetch" "\s\+[a-f0-9]\{7\}\.\.[a-f0-9]\{7\}" ; then
+      echo "$fetch"
+      git_update "$dir" "$git_url"
+      return 0
+    fi
+    status=$(cd $dir; command git status )
+    if matches "$status" "Your branch is behind" ; then
+      git_update "$dir" "$git_url"
     fi
   fi
+}
+
+git_update () {
+  [ -n "$2" ] && echo "updating $2"
+  bake_at $1
+  bake "command git pull"
+  bake_at $1
+  bake "command git log HEAD@{1}.."
 }
