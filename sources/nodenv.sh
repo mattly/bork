76e6ad2e4aab8f756473cc30eb4df736fa941447
Matthew Lyon <matthew@lyonheart.us>
2013-11-17 10:53:01 -0200

    cleanup old things to come back to

--- sources/nodenv.sh
+++
@@ -1,6 +0,0 @@
-nodenv () {
-  if ! contains "$(command nodenv versions --bare )" $1; then
-    bake "command nodenv install $1"
-  fi
-}
-
--- sources/rbenv.sh
+++
@@ -1,7 +0,0 @@
-rbenv () {
-  if ! contains "$(command rbenv versions --bare )" $1; then
-    bake "command rbenv install $1"
-  fi
-}
-
-
