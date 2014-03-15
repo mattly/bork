13c7b269ac76206535c218e25cb241e2665b1cce
Matthew Lyon <matthew@lyonheart.us>
2014-03-15 16:38:39 -0700

    brew pkg: knows if its outdated and how to update
    - "status" checks brew's git to see if its outdated, since master doesn't track
      we have to manually check the logs.  Returns usual status 11 if outdated.
    
    - "upgrade" will run "brew update"

--- packages/brew.sh
+++ packages/brew.sh
@@ -1,13 +1,21 @@
-case bork_mode in
+cmd="command brew"
+
+case $1 in
   depends)
     echo "platform: Darwin"
     echo "exec: ruby"
     ;;
   status)
     has_exec "brew"
-    [ "$?" -eq 0 ] && return 0 || return 10
+    [ "$?" -gt 0 ] && return 10
+    changes=$(cd /usr/local; git fetch --quiet; git log master..origin/master)
+    [ "$(echo $changes | wc -l | awk '{print $1}')" -gt 0 ] && return 11
     ;;
-  # satisfy)
+  # install)
     # bake --eval 'ruby -e "$(curl -fsSL https://raw.github.com/mxcl/homebrew/go)"'
     # ;;
+  upgrade)
+    bake "$cmd update"
+    ;;
+  *) return 1 ;;
 esac
