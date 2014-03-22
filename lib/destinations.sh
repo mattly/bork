3a01da62be335bddc5ad595bc2c0d2e256fb0193
Matthew Lyon <matthew@lyonheart.us>
2014-03-21 19:12:13 -0700

    destinations: cd into dest directory

--- lib/destinations.sh
+++ lib/destinations.sh
@@ -1,16 +1,28 @@
+bork_destinations_root=$PWD
 bork_destinations=( )
 
 destination () {
   (( last = ${#bork_destinations[*]} - 1 ))
   if [ -z "$1" ]; then
-    if [ $last -eq -1 ]; then echo $PWD
+    if [ $last -eq -1 ]; then echo $bork_destinations_root
     else echo ${bork_destinations[$last]}
     fi
   else
     case "$1" in
-      clear) bork_destinations=( ) ;;
-      pop) unset bork_destinations[$last] ;;
-      push) bork_destinations[$last+1]=$2 ;;
+      clear)
+        bork_destinations=( )
+        cd $(destination)
+        ;;
+      pop)
+        unset bork_destinations[$last]
+        stat=$?
+        cd $(destination)
+        return $stat
+        ;;
+      push)
+        bork_destinations[$last+1]=$2
+        cd $2
+        ;;
       size) echo ${#bork_destinations[*]} ;;
       *) return 1 ;;
     esac
--- test/fn-destinations.sh
+++ test/fn-destinations.sh
@@ -2,25 +2,41 @@
 
 . test/helpers.sh
 
+cwd=
+setup () {
+  cwd=$PWD
+}
+
 @test "it maintains a stack of destinations" {
-  [ "$(destination)" = $PWD ]
+  [ "$(destination)" = $cwd ]
   [ "$(destination size)" -eq 0 ]
+
   destination push $HOME
   [ "$(destination)" = $HOME ]
+  [ $PWD = $HOME ]
   [ "$(destination size)" -eq 1 ]
-  destination push $PWD/.git
-  [ "$(destination)" = "$PWD/.git" ]
+
+  destination push $cwd/.git
+  [ "$(destination)" = "$cwd/.git" ]
+  [ $PWD = "$cwd/.git" ]
   [ "$(destination size)" -eq 2 ]
+
   destination pop
   [ "$(destination)" = $HOME ]
+  [ $PWD = $HOME ]
   [ "$(destination size)" -eq 1 ]
+
   destination pop
-  [ "$(destination)" = $PWD ]
+  [ "$(destination)" = $cwd ]
+  [ $PWD = "$cwd" ]
   [ "$(destination size)" -eq 0 ]
+
   destination push $HOME
   [ "$(destination size)" -eq 1 ]
+
   destination clear
   [ "$(destination size)" -eq 0 ]
+  [ "$PWD" = "$cwd" ]
 }
 
 @test "it returns 1 on unknown command" {
