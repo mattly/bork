e298719a64de2dd5a3c05784a84cc7fe8e73a0cb
Matthew Lyon <matthew@lyonheart.us>
2014-04-19 16:50:59 -0700

    assert user: groups

--- core/user.sh
+++ core/user.sh
@@ -3,6 +3,7 @@ handle=$2
 shift 2
 
 shell=$(arguments get shell $*)
+groups=$(arguments get groups $*)
 
 users_list () {
   if [ -n "$user_list_cmd" ]; then $user_list_cmd
@@ -16,10 +17,34 @@ user_get () {
   echo $row
   return $stat
 }
+
 user_shell () {
   current_shell=$(echo "$1" | cut -d: -f 7)
   if [ "$current_shell" != $2 ]; then
-    echo "--shell: expected $shell, was $current_shell"
+    echo $current_shell
+    return 1
+  fi
+  return 0
+}
+
+user_groups () {
+  if [ -n "$user_groups_cmd" ]; then current=$($user_groups_cmd "$1")
+  else current=$(groups $1)
+  fi
+  case $platform in
+    Linux) current=$(echo "$current" | cut -d: -f 2) ;;
+  esac
+  missing=
+  expected=$(IFS=','; echo $groups)
+  for group in $expected; do
+    echo "$current" | grep -E "\b$group\b"
+    if [ "$?" -gt 0 ]; then
+      [ -n "$missing" ] && missing="$missing,$group"
+      [ -z "$missing" ] && missing=$group
+    fi
+  done
+  if [ -n "$missing" ]; then
+    echo $missing
     return 1
   fi
   return 0
@@ -27,24 +52,41 @@ user_shell () {
 
 case $action in
   status)
+    outdated=
     row=$(user_get $handle)
     [ "$?" -gt 0 ] && return 10
     if [ -n "$shell" ]; then
-      msg=$(user_shell "$row" $shell)
+      current=$(user_shell "$row" $shell)
       if [ "$?" -gt 0 ]; then
-        echo "$msg"
-        return 11
+        echo "--shell: expected $shell; is $current"
+        outdated=1
       fi
     fi
+    if [ -n "$groups" ]; then
+      missing=$(user_groups $handle $groups)
+      if [ "$?" -gt 0 ]; then
+        echo "--groups: expected $groups; missing $missing"
+        outdated=1
+      fi
+    fi
+    [ "$outdated" -gt 0 ] && return 11
     return 0 ;;
   install)
     args="-m"
     [ -n "$shell" ] && args="$args --shell $shell"
+    [ -n "$groups" ] && args="$args --groups $groups"
     bake "useradd $args $handle"
     ;;
   upgrade)
     if ! user_shell $(user_get $handle) $shell ; then
       bake "chsh -s $shell $handle"
+    fi
+    missing=$(user_groups $handle $groups)
+    if [ "$?" -gt 0 ]; then
+      groups_to_create=$(IFS=','; echo $missing)
+      for group in $groups_to_create; do
+        bake "useradd $handle $group"
+      done
     fi
     ;;
   *) return 1 ;;
--- test/assert-user.sh
+++ test/assert-user.sh
@@ -7,8 +7,18 @@ list_users () {
   echo "existant:*:100:100::/home/existant:/bin/bash"
 }
 
+list_groups_for_user () {
+  prefix=
+  [ "$platform" = "Linux" ] && prefix="$1 : "
+  case $1 in
+    existant) echo "bee existant " ;;
+    *) return 1 ;;
+  esac
+}
+
 setup () {
   user_list_cmd="list_users"
+  user_groups_cmd="list_groups_for_user"
 }
 
 # --- without arguments ----------------------------------------
@@ -60,4 +70,46 @@ setup () {
   run baked_output
   [ "${#lines[*]}" -eq 1 ]
   [ "${lines[0]}" = "chsh -s /bin/zsh existant" ]
+}
+
+# --- with group argument ------------------------------------
+@test "user status: with group, returns 10 when user doesn't exist" {
+  run user status nonexistant --groups=foo,bar
+  [ "$status" -eq 10 ]
+}
+
+@test "user status: with group, returns 11 when user belongs to none" {
+  run user status existant --groups=foo,bar
+  p $status
+  [ "$status" -eq 11 ]
+  [ "${#lines[*]}" -eq 1 ]
+  echo "${lines[0]}" | grep -E "^--groups:" >/dev/null
+  echo "${lines[0]}" | grep -E "foo,bar$" >/dev/null
+}
+
+@test "user status: with group, returns 11 when user belongs to some" {
+  run user status existant --groups=foo,bar,bee
+  [ "$status" -eq 11 ]
+}
+
+@test "user status: with group, returns 0 when user belongs to all" {
+  run user status existant --groups=existant,bee
+  [ "$status" -eq 0 ]
+}
+
+@test "user install: with group, bakes 'useradd' with --groups" {
+  run user install nonexistant --groups=foo,bar
+  [ "$status" -eq 0 ]
+  run baked_output
+  [ "${#lines[*]}" -eq 1 ]
+  [ "${lines[0]}" = "useradd -m --groups foo,bar nonexistant" ]
+}
+
+@test "user upgrade: with group, bakes 'adduser' with user and group for each group" {
+  run user upgrade existant --groups=foo,bar
+  [ "$status" -eq 0 ]
+  run baked_output
+  [ "${#lines[*]}" -eq 2 ]
+  [ "${lines[0]}" = "useradd existant foo" ]
+  [ "${lines[1]}" = "useradd existant bar" ]
 }
