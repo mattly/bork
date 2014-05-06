f5f059e0f104063870cf493fddeefba17ac75638
Matthew Lyon <matthew@lyonheart.us>
2014-05-06 09:03:03 -0700

    runner: clean up a bit
    eventually we'll have like, bork-compile.sh and whatnot.

--- lib/runner.sh
+++ lib/runner.sh
@@ -1,17 +1,6 @@
 runner () {
   operation=$1
 
-  case $operation in
-    status | satisfy | compile)
-      if [ ! -e "$2" ]; then
-        echo "bork: $1 command requires a config script"
-        exit 1
-      fi ;;
-    *)
-      echo "bork: must give 'status' or 'satisfy' as first argument"
-      exit 1 ;;
-  esac
-
   # used by include to find 'include foo/bar.sh'
   BORK_SCRIPT_DIR=$(getDir $(pwd -P)/$2)
   BORK_WORKING_DIR=$PWD
@@ -19,5 +8,18 @@ runner () {
   case "$operation" in
     compile) base_compile $2 ;;
     satisfy | status) . $2 ;;
+    *) cat <<END
+bork usage:
+
+bork operation [config-file]
+
+where "operatiion" is one of:
+
+status: determine if the config file's conditions are met
+satisfy: satisfy the config file's conditions if possible
+compile: compile the config file to a self-contained script output to STDOUT
+END
+    exit 1
+    ;;
   esac
 }
