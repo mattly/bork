648c080abf9482f82bfb38d970ae11eb4cdc7b98
Matthew Lyon <matthew@lyonheart.us>
2014-05-11 15:56:20 -0700

    ok uses new status codes

--- lib/declarations/ok.sh
+++ lib/declarations/ok.sh
@@ -1,13 +1,3 @@
-_status_for () {
-  case "$1" in
-    "0")  echo "ok" ;;
-    "10") echo "missing" ;;
-    "11") echo "outdated" ;;
-    "20") echo "conflict" ;;
-    *)    echo "unknown status: $1" ;;
-  esac
-}
-
 _ok_run () {
   fn=$1
   shift
@@ -50,12 +40,12 @@ ok () {
           _ok_run $fn install $*
           _changes_complete $? 'install'
           ;;
-        11)
+        11|12|13)
           echo "$status_output"
           _ok_run $fn upgrade $*
           _changes_complete $? 'upgrade'
           ;;
-        20)
+        *)
           echo "* $status_output"
           ;;
       esac
--- lib/helpers/status_codes.sh
+++ lib/helpers/status_codes.sh
@@ -7,8 +7,29 @@ STATUS_MISMATCH_CLOBBER=14
 STATUS_CONFLICT_UPGRADE=20
 STATUS_CONFLICT_CLOBBER=21
 STATUS_CONFLICT_HALT=25
-STATUS_ERROR_BAD_ARGUMENTS=30
-STATUS_ERROR_FAILED_ARGUMENTS=31
+STATUS_BAD_ARGUMENTS=30
+STATUS_FAILED_ARGUMENTS=31
 STATUS_FAILED_ARGUMENT_PRECONDITION=32
 STATUS_FAILED_PRECONDITION=33
 STATUS_UNSUPPORTED_PLATFORM=34
+
+_status_for () {
+  case "$1" in
+    $STATUS_OK) echo "ok" ;;
+    $STATUS_MISSING) echo "missing" ;;
+    $STATUS_OUTDATED) echo "outdated" ;;
+    $STATUS_PARTIAL) echo "partial" ;;
+    $STATUS_MISMATCH_UPGRADE) echo "mismatch (upgradable)" ;;
+    $STATUS_MISMATCH_CLOBBER) echo "mismatch (clobber required)" ;;
+    $STATUS_CONFLICT_UPGRADE) echo "conflict (upgradable)" ;;
+    $STATUS_CONFLICT_CLOBBER) echo "conflict (clobber required)" ;;
+    $STATUS_CONFLICT_HALT) echo "conflict (unresolvable)" ;;
+    $STATUS_BAD_ARGUMENT) echo "error (bad arguments)" ;;
+    $STATUS_FAILED_ARGUMENTS) echo "error (failed arguments)" ;;
+    $STATUS_FAILED_ARGUMENT_PRECONDITION) echo "error (failed argument precondition)" ;;
+    $STATUS_FAILED_PRECONDITION) echo "error (failed precondition)" ;;
+    $STATUS_UNSUPPORTED_PLATFORM) echo "error (unsupported platform)" ;;
+    *)    echo "unknown status: $1" ;;
+  esac
+}
+
