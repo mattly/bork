ddc45553523e19a93fc30265589f14d63eb2f04e
Matthew Lyon <matthew@lyonheart.us>
2014-05-11 15:42:06 -0700

    git: migrate to new status codes

--- core/git.sh
+++ core/git.sh
@@ -10,14 +10,15 @@ git_dir="$git_name"
 git_branch="master"
 
 case $action in
-  depends) echo "exec: git" ;;
   status)
+    needs_exec "git" || return $STATUS_FAILED_PRECONDITION
+
     # if the directory is missing, it's missing
-    bake [ ! -d $git_dir ] && return 10
+    bake [ ! -d $git_dir ] && return $STATUS_MISSING
 
     # if the directory is present but empty, it's missing
     git_dir_contents=$(str_item_count "$(bake ls -A $git_dir)")
-    [ "$git_dir_contents" -eq 0 ] && return 10
+    [ "$git_dir_contents" -eq 0 ] && return $STATUS_MISSING
 
     bake cd $git_dir
     # fetch from the remote without fast-forwarding
@@ -29,11 +30,11 @@ case $action in
     # If the directory isn't a git repo, conflict
     if [ $git_fetch_status -gt 0 ]; then
       echo "destination directory $git_dir exists, not a git repository (exit status $git_fetch_status)"
-      return 20
+      return $STATUS_CONFLICT_CLOBBER
     elif str_matches "$git_fetch" '"^fatal"'; then 
       echo "destination directory exists, not a git repository"
       echo "$git_fetch"
-      return 20
+      return $STATUS_CONFLICT_CLOBBER
     fi
 
     git_stat=$(bake git status -uno -b --porcelain)
@@ -43,28 +44,28 @@ case $action in
     str_matches "$(str_get_field "$git_first_line" 2)" "$git_branch"
     if [ "$?" -ne 0 ]; then
       echo "local git repository is on incorrect branch"
-      return 20
+      return $STATUS_MISMATCH_UPGRADE
     fi
 
     git_divergence=$(str_get_field "$git_first_line" 3)
     if str_matches "$git_divergence" 'ahead'; then
       echo "local git repository is ahead of remote"
-      return 20
+      return $STATUS_CONFLICT_UPGRADE
     fi
 
     # are there changes?
     # git_change_match="'^\\s\\?\\w'"
-    if str_matches "$git_stat" "^\\s\\?\\w"; then
+    if str_matches "$git_stat" "^\\s?\\w"; then
       echo "local git repository has uncommitted changes"
-      return 20
+      return $STATUS_CONFLICT_UPGRADE
     fi
     # if str_matches "$git_stat" $git_change_match; then return 20; fi
 
     # # If it's known to be behind, outdated
-    if str_matches "$git_divergence" 'behind'; then return 11; fi
+    if str_matches "$git_divergence" 'behind'; then return $STATUS_OUTDATED; fi
 
     # guess we're clean, so things are OK
-    ;;
+    return $STATUS_OK ;;
   install)
     bake mkdir -p $git_dir
     bake git clone $git_url $git_dir
