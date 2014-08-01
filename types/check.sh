c78655c24191554f9fd55e8e54d80a2173f77dad
Matthew Lyon <matthew@lyonheart.us>
2014-07-31 17:08:12 -0700

    new type: check

---
+++ types/check.sh
@@ -0,0 +1,15 @@
+# TODO tests
+action=$1
+shift 1
+case $action in
+  desc)
+    echo "runs a given command.  OK if returns 0, FAILED otherwise."
+    echo '* check evalstr'
+    echo '> check "[ -d $HOME/.ssh/id_rsa ]"'
+    echo '> if check_failed; then ...'
+    ;;
+  status)
+    eval "$*"
+    [ "$?" -gt 0 ] && return $STATUS_FAILED || return $STATUS_OK
+    ;;
+esac
