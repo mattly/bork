c4625a1184a121e78aad6c46f59e779ce2b71679
Matthew Lyon <matthew@lyonheart.us>
2014-10-19 19:06:58 -0700

    Use new homebrew curl location
    missing: brew
    Whoops, the Homebrew installer has moved! Please instead run:
    
    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
    
    Also, please ask wherever you got this link from to update it to the above.
    Thanks!
    verifying : brew
    *  failed

--- types/brew.sh
+++ types/brew.sh
@@ -26,7 +26,7 @@ if [ -z "$name" ]; then
       ;;
 
     install)
-      bake 'ruby -e "$(curl -fsSL https://raw.github.com/mxcl/homebrew/go/install)"'
+      bake 'ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/homebrew/master/install)"'
       ;;
 
     upgrade)
