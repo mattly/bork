db3b4d4c4b51e48a00c2a8408d1d94638c65aaee
Jonas Friedmann <j@frd.mn>
2015-01-31 22:54:42 +0100

    Improve escaping

--- types/symlink.sh
+++ types/symlink.sh
@@ -8,16 +8,16 @@ case "$action" in
     echo "> symlink .vimrc ~/code/dotfiles/configs/vimrc"
     ;;
   status)
-    if bake [ -h $target ]; then
-      existing_source=$(bake readlink $target)
-      if [ "$existing_source" != $source ]; then
+    if bake [ -h "\"$target\"" ]; then
+      existing_source=$(bake readlink \"$target\")
+      if [ "$existing_source" != "$source" ]; then
         echo "received source for existing symlink: $existing_source"
         echo "expected source for symlink: $source"
         return $STATUS_MISMATCH_CLOBBER
       else
         return $STATUS_OK
       fi
-    elif bake [ -e $target ]; then
+    elif bake [ -e "\"$target\"" ]; then
       echo "not a symlink: $target"
       return $STATUS_CONFLICT_CLOBBER
     else
@@ -25,7 +25,7 @@ case "$action" in
     fi ;;
 
   install|upgrade)
-    bake ln -s $source $target ;;
+    bake ln -s "$source" "$target" ;;
 
   *) return 1;;
 esac
