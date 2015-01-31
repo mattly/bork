9a828118d1373840622a43a25e0f0b433442740b
Jonas Friedmann <j@frd.mn>
2015-01-31 21:01:50 +0100

    Add branch argument for github type

--- types/github.sh
+++ types/github.sh
@@ -4,13 +4,22 @@
 action=$1
 repo=$2
 shift 2
+branch=$(arguments get branch $*)
 
 case $action in
   desc)
     echo "front-end for git type, uses github urls"
     echo "> ok github mattly/bork"
+    echo "--branch=gh-pages        (specify branch)"
     ;;
-  compile) include_assertion git $BORK_SOURCE_DIR/types/git.sh ;;
-  *) . $BORK_SOURCE_DIR/types/git.sh $action "https://github.com/$(echo $repo).git" $* ;;
+  compile)
+    include_assertion git $BORK_SOURCE_DIR/types/git.sh
+    ;;
+  *)
+    if [[ -z $branch ]]; then
+      . $BORK_SOURCE_DIR/types/git.sh $action "https://github.com/$(echo $repo).git" $*
+    else
+      . $BORK_SOURCE_DIR/types/git.sh $action "https://github.com/$(echo $repo).git" --branch=$branch $*
+    fi
+    ;;
 esac
-
