0f1d7207a4bbc7949b2254c164cb03523cbfa5a1
Matthew Lyon <matthew@lyonheart.us>
2018-01-25 23:53:02 -0800

    one more try, and fuck you bash

--- types/apk.sh
+++ types/apk.sh
@@ -14,9 +14,7 @@ case "${action}" in
     needs_exec "apk" || return "${STATUS_FAILED_PRECONDITION}"
 
     bake apk info --installed "${name}" || return "${STATUS_MISSING}"
-    o=$(bake apk version | awk '{print $1}' | grep -E "^${name}-\d")
-    [ -n "$o" ] && return "${STATUS_OUTDATED}"
-    # bake apk version | awk '{print $1}' | grep -E "^${name}-\d" > /dev/null && return "${STATUS_OUTDATED}"
+    bake apk version | grep -E "^${name}-\d+\.\d+" > /dev/null && return "${STATUS_OUTDATED}"
     return "${STATUS_OK}"
     ;;
   install|upgrade)
